adec9b652f9dbe8faa67627d59780edf19c52300
Cori1109 <chris.jenkins.j1005@gmail.com>
2022-10-12 10:39:46 +0800

    Add Product Item and define style

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import React from 'react';
+
+interface Product {
+  name: string;
+  slug: string;
+  category: string;
+  image: string;
+  price: number;
+  brand: string;
+  rating: number;
+  numReviews: number;
+  countInStock: number;
+  description: string;
+}
+
+type Props = {
+  product?: Product;
+};
+
+const ProductItem = ({ product }: Props) => {
+  return (
+    <div className="card">
+      <Link href={`/product/${product?.slug}`}>
+        <a>
+          <img
+            src={product?.image}
+            alt={product?.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product?.slug}`}>
+          <a>
+            <h2 className="text-lg">{product?.name}</h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product?.brand}</p>
+        <p>$ {product?.price}</p>
+
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
 import Layout from '@/components/Layout';
+import ProductItem from '@/components/ProductItem';
+import data from '@/utils/data';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
