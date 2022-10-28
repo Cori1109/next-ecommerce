d6f02addd0e4308febbd227f2e6502b50612ee72
Cori1109 <chris.jenkins.j1005@gmail.com>
2022-10-28 18:52:18 +0800

    Add admin orders page

---
+++ src/pages/api/admin/orders.ts
@@ -0,0 +1,25 @@
+import { Order } from 'src/models/Order.entity';
+import db from '@/utils/db';
+import { getSession } from 'next-auth/react';
+
+const handler = async (req, res) => {
+  const session = await getSession({ req });
+  if (!session || (session && !session.user.isAdmin)) {
+    return res.status(401).send('signin required');
+  }
+  if (req.method === 'GET') {
+    await db.connection();
+    // const orders = await Order.find({}).populate('user', 'name');
+
+    const orders = await db.AppDataSource.getMongoRepository(Order).find({
+      relations: ['user', 'name'],
+    });
+
+    await db.disconnection();
+    res.send(orders);
+  } else {
+    return res.status(400).send({ message: 'Method not allowed' });
+  }
+};
+
+export default handler;
